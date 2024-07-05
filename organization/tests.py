a1f6cf011d4a8f51601b0cd9edaddf386b2cf713
Goodnessmbakara <amicablembakara50@gmail.com>
2024-07-05 20:08:54 +0100

    test cases to capture that users can access their own organization but not organization created by other people

--- organization/tests.py
+++ organization/tests.py
@@ -8,33 +8,41 @@ from .models import Organisation
 User = get_user_model()
 
 class OrganisationTests(APITestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(
+    def setUp(self):        
+        self.user1 = User.objects.create_user(
             firstName="John",
             lastName="Doe",
             email="john.doe@example.com",
             password="password123",
             phone="1234567890"
         )
-        self.organisation = Organisation.objects.create(
+        self.organisation1 = Organisation.objects.create(
             name="John's Organisation",
             description="Default organisation for John"
         )
-        self.user.organisations.add(self.organisation)
-        refresh = RefreshToken.for_user(self.user)
-        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(refresh.access_token))
+        self.user1.organisations.add(self.organisation1)
 
-    def test_get_user_details(self):
-        url = reverse('user-detail', args=[self.user.id])
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['data']['email'], "john.doe@example.com")
+        self.user2 = User.objects.create_user(
+            firstName="Jane",
+            lastName="Doe",
+            email="jane.doe@example.com",
+            password="password123",
+            phone="0987654321"
+        )
+        self.organisation2 = Organisation.objects.create(
+            name="Jane's Organisation",
+            description="Default organisation for Jane"
+        )
+        self.user2.organisations.add(self.organisation2)
+
+        self.refresh1 = RefreshToken.for_user(self.user1)
+        self.refresh2 = RefreshToken.for_user(self.user2)
 
     def test_get_organisations(self):
         url = reverse('organisation-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data['data']['organisations']), 1)
+        self.assertEqual(len(response.data['data']['organisations']), 2)
 
     def test_create_organisation(self):
         url = reverse('create-organisation')
@@ -45,3 +53,16 @@ class OrganisationTests(APITestCase):
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['data']['name'], "New Organisation")
+    
+    def test_user_cannot_access_other_organisation(self):
+        # Authenticate as user1
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(self.refresh1.access_token))
+        response = self.client.get(reverse('organisation-detail', args=[self.organisation2.id]))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_can_access_own_organisation(self):
+        # Authenticate as user1
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(self.refresh1.access_token))
+        response = self.client.get(reverse('organisation-detail', args=[self.organisation1.id]))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['data']['name'], "John's Organisation")
