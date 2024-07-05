845baf82f9418e4d125e12531fba785c549e4326
Goodnessmbakara <amicablembakara50@gmail.com>
2024-07-05 19:19:15 +0100

    finished setting up the environment variables now time to add swagger documentation for my project

--- django_user_org_management/settings.py
+++ django_user_org_management/settings.py
@@ -25,10 +25,10 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-6*50hlkh5$l7rxf4@2hq8cz#9_yww$j_myb7&6115m4e)#o@ht"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
