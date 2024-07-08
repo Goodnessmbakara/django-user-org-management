8ce451ff8f43c6f704a1a2ac0107cb4efd8708b7
Goodnessmbakara <amicablembakara50@gmail.com>
2024-07-08 13:59:06 +0100

    all endpoints tested

--- organization/views.py
+++ organization/views.py
@@ -1,9 +1,11 @@
 from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import Organisation
-from .serializers import  OrganisationSerializer
+from .serializers import OrganisationSerializer
+
 
 class OrganisationView(APIView):
     permission_classes = [IsAuthenticated]
@@ -16,12 +18,31 @@ class OrganisationView(APIView):
             'message': 'Organisations retrieved successfully',
             'data': {'organisations': serializer.data}
         }, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = OrganisationSerializer(data=request.data)
+        if serializer.is_valid():
+            organisation = serializer.save()
+            organisation.users.add(request.user)
+            return Response({
+                'status': 'success',
+                'message': 'Organisation created successfully',
+                'data': serializer.data
+            }, status=status.HTTP_201_CREATED)
+        return Response({
+            'status': 'Bad Request',
+            'message': 'Client error',
+            'statusCode': 400,
+            'errors': [
+                {'field': k, 'message': v[0]} for k, v in serializer.errors.items()
+            ]
+        }, status=status.HTTP_400_BAD_REQUEST)
 
 class SingleOrganisationView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, orgId):
-        organisation = Organisation.objects.filter(org_id=orgId, users=request.user).first()
+        organisation = Organisation.objects.filter(orgId=orgId, users=request.user).first()
         if organisation:
             serializer = OrganisationSerializer(organisation)
             return Response({
