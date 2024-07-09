1edcb66e0b9df122df83a98bdc82cb58660038f3
Goodnessmbakara <amicablembakara50@gmail.com>
2024-07-09 11:27:35 +0100

    import error fixed

--- organization/views.py
+++ organization/views.py
@@ -2,11 +2,11 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from django.contrib.auth import get_user_model
 from .models import Organisation
 from .serializers import OrganisationSerializer
 
-
+User = get_user_model()
 class OrganisationView(APIView):
     permission_classes = [IsAuthenticated]
 
